f1a1dae1e67d14e0571c170b86b5575749c0bc71
Gunmo Koo <minju1348@gmail.com>
2022-01-08 23:47:09 +0900

    Create Account

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import AppRouter from "components/Router";
 import { authService } from "fbase";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
   return (
     <>
       <AppRouter isLoggedIn={isLoggedIn} />
